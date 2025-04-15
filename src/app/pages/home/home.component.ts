569029713fae7d677e81d68a2f1c4c2c2978fd61
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-15 19:14:16 +0530

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CarouselComponent } from "../carousel/carousel/carousel.component";
+import { CarouselComponent } from "./carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
