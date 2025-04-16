b29816f1d27ad6042234a0d41b6fecf809b9d4cf
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-16 13:34:12 +0530

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoveToTopComponent } from './movetotop/movetotop.component';
 import { HeaderComponent } from '../../layout/header/header.component';
+import { FooterComponent } from "../../layout/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MoveToTopComponent, HeaderComponent],
+  imports: [CommonModule, MoveToTopComponent, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
