201353882071672868185612b44f7891536c38b6
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-18 00:41:21 +0530

    cc

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -23,11 +23,6 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   ngOnInit(): void { }
 
   showMobileNav = false;
-  touchStartX = 0;
-  touchEndX = 0;
-  currentIndex: number = 0;
-  autoScrollInterval: any;
-  autoScrollTimeout: any;
 
   @ViewChild('lightGallery', { static: false }) lightGallery!: ElementRef;
 
@@ -41,107 +36,20 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     'https://images.unsplash.com/photo-1454023492550-5696f8ff10e1?auto=format&fit=crop&w=800&q=80'
   ];
 
-  get currentImage(): string {
-    return this.images[this.currentIndex];
-  }
-
-  prevImage() {
-    if (this.autoScrollInterval) {
-      clearInterval(this.autoScrollInterval);
-      this.autoScrollInterval = null;
-    }
-    if (this.autoScrollTimeout) {
-      clearTimeout(this.autoScrollTimeout);
-    }
-    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-    if (this.modalOpen) {
-      this.modalImage = this.images[this.currentIndex];
-    }
-    // Resume auto-scroll after 3 seconds
-    this.autoScrollTimeout = setTimeout(() => {
-      if (!this.autoScrollInterval) {
-        this.autoScrollInterval = setInterval(() => {
-          this.nextImage();
-        }, 2000);
-      }
-    }, 300);
-  }
-
-  nextImage() {
-    if (this.autoScrollInterval) {
-      clearInterval(this.autoScrollInterval);
-      this.autoScrollInterval = null;
-    }
-    if (this.autoScrollTimeout) {
-      clearTimeout(this.autoScrollTimeout);
-    }
-    this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    if (this.modalOpen) {
-      this.modalImage = this.images[this.currentIndex];
-    }
-    // Resume auto-scroll after 3 seconds
-    this.autoScrollTimeout = setTimeout(() => {
-      if (!this.autoScrollInterval) {
-        this.autoScrollInterval = setInterval(() => {
-          this.nextImage();
-        }, 2000);
-      }
-    }, 300);
-  }
-
-
-  // Swipe for main image row
-  onTouchStart(event: TouchEvent) {
-    this.touchStartX = event.changedTouches[0].screenX;
-    // Pause auto image scrolling while swiping
-    if (this.autoScrollInterval) {
-      clearInterval(this.autoScrollInterval);
-      this.autoScrollInterval = null;
-    }
-  }
-
-  onTouchMove(event: TouchEvent) {
-    this.touchEndX = event.changedTouches[0].screenX;
-  }
-
-  onTouchEnd(event: TouchEvent) {
-    const deltaX = this.touchEndX - this.touchStartX;
-    if (Math.abs(deltaX) > 50) { // Minimum swipe distance
-      if (deltaX < 0) {
-        this.nextImage();
-      } else {
-        this.prevImage();
-      }
-    }
-    // Resume auto image scrolling after swipe
-    if (!this.autoScrollInterval) {
-      this.autoScrollInterval = setInterval(() => {
-        this.nextImage();
-      }, 2000);
-    }
-  }
 
   ngAfterViewInit(): void {
     if (this.lightGallery) {
-      // Cast config to 'any' to allow plugin-specific options (autoplayInterval)
       this.lgInstance = lightGallery(this.lightGallery.nativeElement, {
         plugins: [lgThumbnail, lgZoom, lgFullscreen, lgAutoplay],
-        speed: 600, // Smoother transition
+        speed: 600,
         thumbnail: true,
         zoom: false,
         autoplay: true,
         autoplayControls: true,
-        autoplayInterval: 2000, // ms between slides
-        mode: 'lg-slide' // Smooth sliding transition
+        autoplayInterval: 2000,
+        mode: 'lg-slide'
       } as any);
     }
-    // Start auto slideshow
-    if (this.autoScrollInterval) {
-      clearInterval(this.autoScrollInterval);
-    }
-    this.autoScrollInterval = setInterval(() => {
-      this.nextImage();
-    }, 2000); // 2000 ms = 2 seconds
   }
 
   openGalleryAt(index: number) {
