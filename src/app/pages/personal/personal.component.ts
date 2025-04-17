ef88d2fb7bbb0bf7c23977ef52b63d17a6ecdaf8
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-17 14:13:56 +0530

    c

--- src/app/pages/personal/personal.component.ts
+++ src/app/pages/personal/personal.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-personal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './personal.component.html',
   styleUrl: './personal.component.scss'
 })
 export class PersonalComponent {
+  projects = [
+    {
+      image: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80',
+      caption: "I'm an image title."
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=800&q=80',
+      caption: "I'm an image title."
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=800&q=80',
+      caption: "I'm an image title."
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?auto=format&fit=crop&w=800&q=80',
+      caption: "I'm an image title."
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1454023492550-5696f8ff10e1?auto=format&fit=crop&w=800&q=80',
+      caption: "I'm an image title."
+    }
+  ];
 
 }
