3c1512a7ff7c8605e4171d03520169c97bb7c391
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-17 14:32:47 +0530

    c

--- src/app/pages/personal/personal.component.ts
+++ src/app/pages/personal/personal.component.ts
@@ -12,23 +12,33 @@ export class PersonalComponent {
   projects = [
     {
       image: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80',
-      caption: "I'm an image title."
+      title: 'Project Title',
+      caption: "I'm an image title.",
+      moreText: 'more...'
     },
     {
       image: 'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=800&q=80',
-      caption: "I'm an image title."
+      title: 'Project Title',
+      caption: "I'm an image title.",
+      moreText: 'more...'
     },
     {
       image: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=800&q=80',
-      caption: "I'm an image title."
+      title: 'Project Title',
+      caption: "I'm an image title.",
+      moreText: 'more...'
     },
     {
       image: 'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?auto=format&fit=crop&w=800&q=80',
-      caption: "I'm an image title."
+      title: 'Project Title',
+      caption: "I'm an image title.",
+      moreText: 'more...'
     },
     {
       image: 'https://images.unsplash.com/photo-1454023492550-5696f8ff10e1?auto=format&fit=crop&w=800&q=80',
-      caption: "I'm an image title."
+      title: 'Project Title',
+      caption: "I'm an image title.",
+      moreText: 'more...'
     }
   ];
 
