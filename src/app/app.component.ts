e78ea02b8e1cdfa12d529ee8aa855f93b4cffcfa
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-18 22:46:39 +0530

    C

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from "./layout/footer/footer.component";
 import { HeaderComponent } from "./layout/header/header.component";
+import { SecondHeaderComponent } from "./layout/second-header/second-header/second-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, FooterComponent, HeaderComponent, SecondHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/layout/second-header/second-header/second-header.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-second-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './second-header.component.html',
+  styleUrl: './second-header.component.scss'
+})
+export class SecondHeaderComponent {
+  secondHeaderTitle = 'ABINASH MOHANTA';
+
+  private routeTitleMap: { [key: string]: string } = {
+    home: 'ABINASH MOHANTA',
+    about: 'ABOUT ME',
+    contact: 'CONTACT',
+    personal: 'PERSONAL PROJECTS',
+    commercial: 'COMMERCIAL PROJECTS',
+    reviews: ' CLIENT REVIEWS',
+  };
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      const url = event.urlAfterRedirects || event.url;
+      const path = url.split('/')[1] || 'home';
+      this.secondHeaderTitle = this.routeTitleMap[path] || 'ABINASH MOHANTA';
+    });
+  }
+}
