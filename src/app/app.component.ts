dfbb11cece4aa291f86bea682d3c519aef0a0a5c
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-19 00:20:00 +0530

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from "./layout/footer/footer.component";
 import { HeaderComponent } from "./layout/header/header.component";
 import { SecondHeaderComponent } from "./layout/second-header/second-header/second-header.component";
+import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
 import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, FooterComponent, HeaderComponent, SecondHeaderComponent],
+  imports: [RouterOutlet, RouterModule, FooterComponent, HeaderComponent, SecondHeaderComponent, ProgressBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
@@ -24,7 +25,52 @@ import { trigger, transition, style, animate } from '@angular/animations';
 export class AppComponent {
   title = 'abinash-mohanta-website';
 
+  progress = 0;
+  progressVisible = false;
+  progressTimer: any = null;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.showProgressBar();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.completeProgressBar();
+      }
+    });
+  }
+
+  showProgressBar() {
+    this.progress = 0;
+    this.progressVisible = true;
+    this.increaseProgress();
+  }
+
+  increaseProgress() {
+    if (this.progress < 90) {
+      this.progress += Math.random() * 10 + 5;
+      this.progress = Math.min(this.progress, 90);
+      this.progressTimer = setTimeout(() => this.increaseProgress(), 200);
+    }
+  }
+
+  completeProgressBar() {
+    this.progress = 100;
+    setTimeout(() => {
+      this.progressVisible = false;
+      this.progress = 0;
+      if (this.progressTimer) {
+        clearTimeout(this.progressTimer);
+        this.progressTimer = null;
+      }
+    }, 400);
+  }
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.isActivated ? outlet.activatedRoute : '';
   }
 }
+
