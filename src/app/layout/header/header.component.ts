8a2c9c012aaee7fd069fcc289b9d6c23b64dcef1
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-20 02:17:02 +0530

    c

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent {
   showMobileNav = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  @HostBinding('class.scrolled') isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  @HostListener('window:touchmove', [])
+  @HostListener('window:orientationchange', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 2;
+    this.cdr.markForCheck();
+  }
 
   closeMobileNav() {
     this.showMobileNav = false;
