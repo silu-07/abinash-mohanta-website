75366102c24d49a8d3115cbcbc4de279781a9b4d
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-22 23:12:12 +0530

    c

--- src/app/shared/modal/gallery-modal/gallery-modal.component.ts
+++ src/app/shared/modal/gallery-modal/gallery-modal.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-gallery-modal',
@@ -8,13 +8,46 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './gallery-modal.component.html',
   styleUrls: ['./gallery-modal.component.scss']
 })
-export class GalleryModalComponent {
+export class GalleryModalComponent implements OnInit {
+  isFullScreen = false;
+
+  ngOnInit(): void {}
+
   @Input() images: string[] = [];
   @Input() open = false;
   @Input() initialIndex = 0;
   @Output() close = new EventEmitter<void>();
 
   currentIndex = 0;
+
+  toggleFullScreen() {
+    const modal = document.querySelector('.gallery-modal') as HTMLElement;
+    if (!modal) return;
+
+    if (!this.isFullScreen) {
+      if (modal.requestFullscreen) {
+        modal.requestFullscreen();
+      } else if ((modal as any).webkitRequestFullscreen) {
+        (modal as any).webkitRequestFullscreen();
+      } else if ((modal as any).msRequestFullscreen) {
+        (modal as any).msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if ((document as any).webkitExitFullscreen) {
+        (document as any).webkitExitFullscreen();
+      } else if ((document as any).msExitFullscreen) {
+        (document as any).msExitFullscreen();
+      }
+    }
+  }
+
+  constructor() {
+    document.addEventListener('fullscreenchange', () => {
+      this.isFullScreen = !!document.fullscreenElement;
+    });
+  }
 
   ngOnChanges() {
     if (this.open) {
