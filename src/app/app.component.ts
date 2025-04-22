4fa91d10895505f96afe137930f67cb52c63edf1
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-04-22 14:46:42 +0530

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Router, RouterOutlet, NavigationStart, NavigationEnd, NavigationCancel,
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from "./layout/footer/footer.component";
 import { HeaderComponent } from "./layout/header/header.component";
-import { SecondHeaderComponent } from "./layout/second-header/second-header.component";
+import { SecondHeaderComponent } from "./shared/second-header/second-header.component";
 import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
 import { trigger, transition, style, animate } from '@angular/animations';
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { MoveToTopComponent } from './movetotop/movetotop.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { ModalComponent } from './modal/card-modal/modal.component';
 import { SearchFilterComponent } from './search-filter/search-filter.component';
+import { SecondHeaderComponent } from './second-header/second-header.component';
 
 @NgModule({
-  imports: [CommonModule, MoveToTopComponent, ProgressBarComponent, ModalComponent, SearchFilterComponent],
-  exports: [MoveToTopComponent, ProgressBarComponent, ModalComponent, SearchFilterComponent]
+  imports: [CommonModule, MoveToTopComponent, ProgressBarComponent, ModalComponent, SearchFilterComponent, SecondHeaderComponent],
+  exports: [MoveToTopComponent, ProgressBarComponent, ModalComponent, SearchFilterComponent, SecondHeaderComponent]
 })
 export class SharedModule {}
