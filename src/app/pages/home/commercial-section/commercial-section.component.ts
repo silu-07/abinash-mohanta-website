02aa9ea98badc860a9d96cc62312d07e8c6dd0c1
abinash Mohanta <90642615+silu-07@users.noreply.github.com>
2025-05-20 14:49:53 +0530

    c

--- src/app/pages/home/commercial-section/commercial-section.component.ts
+++ src/app/pages/home/commercial-section/commercial-section.component.ts
@@ -10,10 +10,10 @@ import { Component } from '@angular/core';
 })
 export class CommercialSectionComponent {
   photographyImages = [
-    { src: '/carosuel-home/image1.jpg', alt: 'Landscape', title: 'Landscape' },
-    { src: '/carosuel-home/image2.jpg', alt: 'Portrait', title: 'Portrait' },
-    { src: '/carosuel-home/image3.jpg', alt: 'Wildlife', title: 'Wildlife' },
-    { src: '/carosuel-home/image1.jpg', alt: 'Urban', title: 'Urban' }
+    { src: 'carosuel-home/image1.jpg', alt: 'Landscape', title: 'Landscape' },
+    { src: 'carosuel-home/image2.jpg', alt: 'Portrait', title: 'Portrait' },
+    { src: 'carosuel-home/image3.jpg', alt: 'Wildlife', title: 'Wildlife' },
+    { src: 'carosuel-home/image1.jpg', alt: 'Urban', title: 'Urban' }
   ];
   
   onViewMore() {
